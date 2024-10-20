beb2ba427df8f4fed1b095a66e378d08f441c03d
prayag choudhary <154533434+Prayag-choudhary@users.noreply.github.com>
2024-10-20 12:01:28 +0530

    Add files via upload

---
+++ cpp2/algolution-set-3/question5.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+using namespace std;
+bool checkValidity(string s){
+    int low = 0;
+    int high = 0;
+    for(char c:s){
+        if(c=='(' || c=='[' || c=='{' ){
+            high++;
+            low++;
+        }
+        else if(c==')' || c==']' || c=='}' ){
+            high--;
+            low--;
+        }
+        else{
+            high++;
+            low--;
+        }
+        if(high<0){
+            return false;
+        }
+    }
+    return low==0;
+}
+int main(){
+    string s="([])";
+    bool isValid = checkValidity(s);
+    if(isValid){
+        cout<<"string is valid";
+    }
+    else{
+        cout<<"string is invalid";
+    }
+}
